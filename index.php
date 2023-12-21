9e37bc733b2c6b7965fefb1212a2eb856fbcb9c2
Daniela Cantillo <72877179+Dee-17@users.noreply.github.com>
2023-12-21 20:23:09 +0800

    Update README.md

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>C597 Corporation</title>
+    <title>LOCALBIMA</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous" defer></script>
     <link rel="stylesheet" href="pages/css/landing-page.css">
@@ -15,9 +15,9 @@
                 <!-- Corporation Intro -->
                 <div class="left_panel m-0 p-5 col col-6">
                     <div class="py-5 m-0">
-                        <img class="mb-4" src="pages/images/company-logo.png" alt="C597 Corporation Logo">
+                        <img class="mb-4" src="pages/images/company-logo.png" alt="LOCALBIMA Logo">
                         <h3 class="header_title">Employee<br>Attendance<br>System</h3>
-                        <p class="footer_title mt-5">C597 Corporation | All Rights Reserved</p>
+                        <p class="footer_title mt-5">LOCALBIMA | All Rights Reserved</p>
                     </div>
                 </div>
                 <!-- Position selection -->
